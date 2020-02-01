44426f02708d025f7d4765fd913d6264811cf6b5
ogbeche77 <ogbeche77@yahoo.com>
2020-02-01 13:51:39 +0100

    styled

--- Weather APP/script.js
+++ Weather APP/script.js
@@ -1,3 +1,15 @@
-function tobi (wea){
-    alert
-}
+window.addEventListener("load", ()=>{ // used to get location after page has loaded
+    let long;
+    let lat;
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position =>{ //inbuilt javascript function that retrieves location, lat. & long.
+            long = position.coords.longitude
+            lat = position.coords.latitude
+                //api gotten from darksky.net then quoted backwards, geolocation also changed to ${lat},${long}
+            const api = `https://api.darksky.net/forecast/f45fd163aefb3c633f0a30138a25fe5b/${lat},${long}`;
+        });
+
+    }
+
+});
